b03a236aa82ec9566c1ce11a71d69ff2969b1703
pedro henrique <pedibr5@gmail.com>
2025-11-13 16:13:51 -0300

    Ambas telas de histórico concluídas, sistema de salvamento local de histórico de rotas por meio do Hive

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:safeway/core/configs/app_router.dart';
-import 'package:safeway/core/configs/firebase_options_dev.dart';
-import 'package:safeway/features/navigation/models/lat_lng_adapter.dart';
-import 'package:safeway/features/navigation/models/route_history_model.dart';
-
-import 'core/di/theme_providers.dart';
-import 'core/theme/app_theme.dart';
-
+import 'package
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: const String.fromEnvironment('ENV_FILE', defaultValue: '.env'));
@@ -21,12 +14,13 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+
   final serverClientId = dotenv.env['GOOGLE_SERVER_CLIENT_ID'];
-  await GoogleSignIn.instance.initialize(serverClientId: serverClientId);
+  await GoogleSignIn.instance.initialize(serverClientId);
 
   await Hive.initFlutter();
 
-  Hive.registerAdapter(RouteHistoryModelAdapter());
+  Hive.registerAdapter(RouteHistoryModel());
   Hive.registerAdapter(LatLngAdapter());
 
   await Hive.openBox<RouteHistoryModel>('route_history');
@@ -37,7 +31,7 @@ void main() async {
 }
 
 class SafewayApp extends ConsumerWidget {
-  const SafewayApp({super.key});
+  const SafewayApp({super.key})
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
